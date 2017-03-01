2173f0ae20e9bda0791d38609eb28574f294297e
Ali Shaikh <aliakbarshaikh@live.co.uk>
2017-03-02 01:14:43 +0500

    Added AppDbContext File

---
+++ src/BethanysPieShop/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Pie> Pies { get; set; }
+
+    }
+}
